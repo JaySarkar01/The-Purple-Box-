45775e19f7390de3df190f05dfddec052ce2f9e5
JaySarkar01 <jaysarkari9@gmail.com>
2025-06-23 22:54:24 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ export default function Home() {
   return (
     <div>
       Hi there! This is the home page of your Next.js application.
+      <main>
+        <h1>Welcome to Next.js!</h1>
+        <p>This is a simple example of a Next.js application.</p>
+      </main>
     </div>
   );
 }
