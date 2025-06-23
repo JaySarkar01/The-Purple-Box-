cc7e11699b82928ab57e542765796f489a5fc61a
JaySarkar01 <jaysarkari9@gmail.com>
2025-06-23 22:59:13 +0530

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,20 @@
 "use client";
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 p-4 z-100">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-lg font-bold">
-          MyApp
+        <Link href="/" className="text-white text-lg font-bold flex items-center">
+          <Image
+            src="/assets/logo/logo.png"
+            alt="Logo"
+            width={120}
+            height={120}
+            className="inline-block"
+          />
         </Link>
         <div className="space-x-4">
           <Link href="/" className="text-gray-300 hover:text-white">
