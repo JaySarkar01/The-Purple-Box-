7d9711ca9a466e6142e6bbb60e3ba518b857c7ae
JaySarkar01 <jaysarkari9@gmail.com>
2025-06-24 23:41:48 +0530

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4 z-100">
+    <nav className="bg-white p-4 z-100">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-white text-lg font-bold flex items-center">
+        <Link href="/" className="flex items-center">
           <Image
             src="/assets/logo/logo.png"
             alt="Logo"
@@ -17,13 +17,18 @@ const Navbar = () => {
           />
         </Link>
         <div className="space-x-4">
-          <Link href="/" className="text-gray-300 hover:text-white">
-            Home
+          <Link href="/" className="text-gray-900 hover:text-gray-700">
+            Home <Image
+              src="/assets/vectors/Down_arrow.png"
+              alt="Down Logo"
+              width={10}
+              height={10}
+              className="inline-block"/>
           </Link>
-          <Link href="/about" className="text-gray-300 hover:text-white">
+          <Link href="/about" className="text-gray-900 hover:text-gray-700">
             About
           </Link>
-          <Link href="/contact" className="text-gray-300 hover:text-white">
+          <Link href="/contact" className="text-gray-900 hover:text-gray-700">
             Contact
           </Link>
         </div>
